6f46579b8a3ad5a551153bc0504272f59e53384a
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-06-26 14:22:46 -0400

    added script

--- Scripts/core/app.js
+++ Scripts/core/app.js
@@ -1,5 +1,9 @@
 let core;
+// core module - IIFE
 (function(core) {
+  // App variables
+  let myObject;
+
   /**
    * This function is used for Intialization
    */
@@ -8,6 +12,55 @@ let core;
       `%c App Initializing...`,
       "font-weight: bold; font-size: 20px;"
     );
+
+    myObject = {}; // object literal notation
+    //myObject = new Object(); // constructor notation
+
+    //myObject = Object.create(null);
+
+    /* 
+    // property definition
+    myObject.name = "";
+    myObject.age = 0;
+    */
+
+    // private properties
+    Object.defineProperty(myObject, "_name", {
+      configurable: false,
+      enumerable: false,
+      value: "",
+      writable: true
+    });
+
+    Object.defineProperty(myObject, "_age", {
+      configurable: false,
+      enumerable: false,
+      value: 0,
+      writable: true
+    });
+
+    // public properties
+    Object.defineProperty(myObject, "name", {
+      configurable: false,
+      enumerable: false,
+      get: function() {
+        return this._name;
+      },
+      set: function(newValue) {
+        this._name = newValue;
+      }
+    });
+
+    Object.defineProperty(myObject, "age", {
+      configurable: false,
+      enumerable: false,
+      get: function() {
+        return this._age;
+      },
+      set: function(newValue) {
+        this._age = newValue;
+      }
+    });
 
     Main();
   }
@@ -18,6 +71,11 @@ let core;
    */
   function Main() {
     console.log(`%c App Started...`, "font-weight: bold; font-size: 20px;");
+
+    myObject.name = "Tom";
+    myObject.age = 49;
+
+    console.log(myObject);
   }
 
   window.addEventListener("load", Start);
